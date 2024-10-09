e0fb6cca7fcb4ae4b3d08614d9230c3962972918
Daniel Olivares <olivares@gonzaga.edu>
2024-10-09 08:55:46 -0700

    Initial Commit of working version. Note that there are errors in this version that need to be fixed.

---
+++ app.js
@@ -0,0 +1,23 @@
+"use strict";
+
+//import modules
+import { addTask, clearInput } from "./todo.js";
+import { saveTasks, loadTasks } from "./storage.js";
+
+//import * as storage from "./storage.js";
+
+//set up DOM references
+const taskList = document.getElementById("task-list");
+const newTaskInput = document.getElementById("new-task");
+const addTaskButton = document.getElementById("add-task");
+
+//load tasks from local storage
+const tasks = loadTasks();
+
+//add event listenr for new task
+addTaskButton.addEventListener("click", () => {
+    const task = newTaskInput.value.trim();
+    tasks.push(task);
+    saveTasks(tasks);
+    clearInput(newTaskInput);
+});
